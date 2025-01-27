87010ae09f53c1889ebd82fba5d151993ea981f1
b4h3ru <Bahiruwalelign@gmail.com>
2025-01-27 08:02:24 -0800

    add

---
+++ resource/views/admin/llinks.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        
+.profile{
+    text-align: right;
+}
+.profile button{
+    background-color: transparent;
+    border: none;
+}
+.profile i{
+    font-size: 40px;
+    color: #000;
+    padding: 10px;
+    margin-right: 10px;
+}
+    </style>
+</head>
+<body>
+<div class="profile">
+            <a  href="profile.php"><i class="fa-solid fa-user"></i></a>
+            <button ><i class="fa-solid fa-power-off"></i></button>
+        </div>
+</body>
+</html>
